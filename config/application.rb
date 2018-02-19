f8d77e892a9e42a7fccf416462091b0416e2f4ea
Vitaliy Kireev <chuvayva@gmail.com>
2018-02-19 15:58:03 +0400

    Procoin name

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module EthereumRubySample2
+module Procoin
   class Application < Rails::Application
     config.generators do |g|
       g.test_framework  false
