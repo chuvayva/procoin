fa3466546a0dbc0fbf5a93655681c665cb23bc29
Vitaliy Kireev <chuvayva@gmail.com>
2018-03-21 18:15:21 +0400

    Transfers for authenticated users

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -1,4 +1,5 @@
 class TransfersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @transfers = current_user.transfers.order(created_at: :desc).preload(:target).decorate
